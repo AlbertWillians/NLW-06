705af3e1892c38fde2f60aadaddd0e26311d2c3a
Albert Willians <albertwillians@hotmail.com>
2021-06-25 17:50:15 -0300

    finalizando o database no firebase

--- aulas/letmeask/src/pages/NewRoom.tsx
+++ aulas/letmeask/src/pages/NewRoom.tsx
@@ -1,16 +1,16 @@
 import { FormEvent, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import illistrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
-import { database } from '../services/firebase'
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 import '../styles/auth.scss'
 
 export function NewRoom() {
    const { user } = useAuth()
-
    const [newRoom, setNewRoom] = useState('')
+   const history = useHistory()
 
    async function handleCreateRoom(event: FormEvent) {
       event.preventDefault()
@@ -25,6 +25,8 @@ export function NewRoom() {
          title: newRoom,
          authorId: user?.id
       })
+
+      history.push(`/rooms/${firebaseRoom.key}`)
    }
 
    return (
